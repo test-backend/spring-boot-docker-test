fd3819af073c23efc20ee917c209eaffedd7bf8e
ltorres <luis.torres@babel.es>
2020-07-18 12:53:11 +0200

    Database scripts

---
+++ src/main/resources/db/migration/V2.1__populate_person_table.sql
@@ -0,0 +1,23 @@
+INSERT INTO PERSON (NAME, BIRTHDAY, LOCATION_ID) VALUES
+    ('Name_1', '1980-03-14', floor(random() * 4 + 1)::int),
+    ('Name_2', '1981-03-14', floor(random() * 4 + 1)::int),
+    ('Name_3', '1982-03-14', floor(random() * 4 + 1)::int),
+    ('Name_4', '1983-03-14', floor(random() * 4 + 1)::int),
+    ('Name_5', '1984-03-14', floor(random() * 4 + 1)::int),
+    ('Name_6', '1985-03-14', floor(random() * 4 + 1)::int),
+    ('Name_7', '1986-03-14', floor(random() * 4 + 1)::int),
+    ('Name_8', '1987-03-14', floor(random() * 4 + 1)::int),
+    ('Name_9', '1988-03-14', floor(random() * 4 + 1)::int),
+    ('Name_10', '1989-03-14', floor(random() * 4 + 1)::int),
+    ('Name_11', '1990-03-14', floor(random() * 4 + 1)::int),
+    ('Name_12', '1991-03-14', floor(random() * 4 + 1)::int),
+    ('Name_13', '1992-03-14', floor(random() * 4 + 1)::int),
+    ('Name_14', '1993-03-14', floor(random() * 4 + 1)::int),
+    ('Name_15', '1994-03-14', floor(random() * 4 + 1)::int),
+    ('Name_16', '1995-03-14', floor(random() * 4 + 1)::int),
+    ('Name_17', '1996-03-14', floor(random() * 4 + 1)::int),
+    ('Name_18', '1997-03-14', floor(random() * 4 + 1)::int),
+    ('Name_19', '1998-03-14', floor(random() * 4 + 1)::int),
+    ('Name_20', '1999-03-14', floor(random() * 4 + 1)::int);
+    
+COMMIT;
